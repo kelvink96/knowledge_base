aff84a8f45f2819ce2bdbdc649c11327c32d8aac
kelvink96 <kelvin.kiptum@alternateltd.com>
2019-09-14 23:28:25 +0300

    editing of an article success

--- app.js
+++ app.js
@@ -34,19 +34,6 @@ app.use(bodyParser.json());
 // set public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// View single article
-app.get('/article/:id', function (req, res) {
-    Article.findById(req.params.id, function (err, article) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('article', {
-                article: article
-            });
-        }
-    })
-});
-
 // Home route
 app.get('/', function (req, res) {
     Article.find({}, function (err, articles) {
@@ -78,7 +65,50 @@ app.post('/articles/add', function (req, res) {
     article.save(function (err) {
         if (err) {
             console.log(err);
-            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
+// View single article
+app.get('/article/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('article', {
+                article: article
+            });
+        }
+    })
+});
+
+// edit article
+app.get('/article/edit/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('edit_article', {
+                title: "Edit Article",
+                article: article
+            })
+        }
+    })
+});
+
+// update post
+app.post('/article/edit/:id', function (req, res) {
+    let article = {};
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    let query = {_id: req.params.id};
+    Article.update(query, article, function (err) {
+        if (err) {
+            console.log(err);
         } else {
             res.redirect('/');
         }
