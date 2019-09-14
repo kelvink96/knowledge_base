613a00e06cc4e136bf5d1914d6b1e40aafc323bc
kelvink96 <kelvin.kiptum@alternateltd.com>
2019-09-14 22:35:46 +0300

    added bower, together with bootstrap and jquery

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/knowledgebase');
 let db = mongoose.connection;
 
 // check connection
 db.once('open', function () {
-   console.log("connected to mongodb")
+    console.log("connected to mongodb")
 });
 
 //check for db errors
 db.on('error', function (err) {
-   console.log(err);
+    console.log(err);
 });
 
 // Init app
 const app = express();
 
+// bring in model
+let Article = require('./models/articles');
+
 // load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false, useNewUrlParser: true}));
+// parse application/json
+app.use(bodyParser.json());
+
+// set public folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Home route
 app.get('/', function (req, res) {
-    let articles = [
-        {
-            id: 1,
-            title: 'Article One',
-            author: 'Kelvin Kiptum',
-            body: 'This is article one'
-        }, {
-            id: 2,
-            title: 'Article Two',
-            author: 'Kelvin Kiptum',
-            body: 'This is article two'
-        }, {
-            id: 3,
-            title: 'Article Three',
-            author: 'Kelvin Kiptum',
-            body: 'This is article three'
+    Article.find({}, function (err, articles) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('index', {
+                title: "Articles",
+                articles: articles
+            });
         }
-    ];
-    res.render('index', {
-        title: "Articles",
-        articles: articles
-    });
+    })
 });
 
 // add articles route
@@ -53,6 +53,23 @@ app.get('/articles/add', function (req, res) {
     res.render('add_articles', {
         title: "Add articles"
     })
+});
+
+// Add submit POST route
+app.post('/articles/add', function (req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
 });
 
 // start server
